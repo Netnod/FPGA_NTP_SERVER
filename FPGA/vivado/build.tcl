3903eb37b6f78709c1f03aeaa9dda3063799126b
Christer Weinigel <wingel@netnod.se>
2019-10-08 14:41:37 +0200

    Try exit with an error status if build failed

--- FPGA/vivado/build.tcl
+++ FPGA/vivado/build.tcl
@@ -6,9 +6,21 @@ reset_run synth_1
 launch_runs synth_1
 wait_on_run synth_1
 
+puts [get_property STATUS [get_runs synth_1]]
+
+if { [get_property STATUS [get_runs synth_1]] != "synth_design Complete!" } {
+    exit 1
+}
+
 # Implement project
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
+
+puts [get_property STATUS [get_runs impl_1]]
+
+if { [get_property STATUS [get_runs impl_1]] != "write_bitstream Complete!" } {
+    exit 1
+}
 
 # Copy .bit file to current directory
 file copy -force ntps/ntps.runs/impl_1/ntps_top.bit ntps_top.bit
