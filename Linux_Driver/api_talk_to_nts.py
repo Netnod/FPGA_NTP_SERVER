5d6edac686eeb9df3b27525912161be119a9971c
Peter Magnusson <blaufish@netnod.se>
2020-02-20 11:20:34 +0100

    api_talk_to_nts.py: multiple NTS engines support

--- Linux_Driver/api_talk_to_nts.py
+++ Linux_Driver/api_talk_to_nts.py
@@ -36,6 +36,7 @@ API_DISPATCHER_ADDR_DUMMY              = 0x003
 API_DISPATCHER_ADDR_SYSTICK32          = 0x004
 API_DISPATCHER_ADDR_NTPTIME            = 0x006
 API_DISPATCHER_ADDR_BYTES_RX           = 0x00a
+API_DISPATCHER_ADDR_NTS_ENGINES_ALL    = 0x011
 API_DISPATCHER_ADDR_COUNTER_FRAMES     = 0x020
 API_DISPATCHER_ADDR_COUNTER_GOOD       = 0x022
 API_DISPATCHER_ADDR_COUNTER_BAD        = 0x024
@@ -135,8 +136,7 @@ def human64(api, base, offset):
     machine = read64(api, base, offset)
     return humanL(machine)
 
-def engine_read32(api, addr):
-    engine = 0 # TODO add as a parameter
+def engine_read32(api, engine, addr):
     id_cmd_addr = (engine<<20) | (BUS_READ<<12) | (addr & 0xFFF)
 
     write32( api, DISPATCHER_BASE, API_DISPATCHER_ADDR_BUS_ID_CMD_ADDR, id_cmd_addr )
@@ -150,8 +150,7 @@ def engine_read32(api, addr):
 
     return result
 
-def engine_write32(api, addr, value):
-    engine = 0 # TODO add as a parameter
+def engine_write32(api, engine, addr, value):
     id_cmd_addr = (engine<<20) | (BUS_WRITE<<12) | (addr & 0xFFF)
 
     write32( api, DISPATCHER_BASE, API_DISPATCHER_ADDR_BUS_ID_CMD_ADDR, id_cmd_addr )
@@ -162,23 +161,35 @@ def engine_write32(api, addr, value):
     while status:
           status = read32( api, DISPATCHER_BASE, API_DISPATCHER_ADDR_BUS_STATUS )
 
-def engine_write32_checkreadback(api, addr, value):
-    engine_write32(api, addr, value)
-    readback = engine_read32(api, addr)
+def engine_write32_checkreadback(api, engine, addr, value):
+    engine_write32(api, engine, addr, value)
+    readback = engine_read32(api, engine, addr)
     if (value != readback):
       raise Exception("WARNING: Write engine[{}]={}, read back was: {}".format(hex(addr), hex(value), hex(readback)));
 
-def engine_read64(api, addr):
-    msb = engine_read32(api, addr)
-    lsb = engine_read32(api, addr + 1)
+def engine_read64(api, engine, addr):
+    msb = engine_read32(api, engine, addr)
+    lsb = engine_read32(api, engine, addr + 1)
     word = (msb<<32) | lsb
     return word
 
-def engine_human32(api, addr):
-    return humanL(engine_read32(api, addr))
+def engine_human32(api, engine, addr):
+    return humanL(engine_read32(api, engine, addr))
 
-def engine_human64(api, addr):
-    return humanL(engine_read64(api, addr))
+def engine_human64(api, engine, addr):
+    return humanL(engine_read64(api, engine, addr))
+
+def detect_engines(api):
+    disp_name = read64(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_NAME)
+    disp_version = read32(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_VERSION)
+    disp_engines = read32(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_NTS_ENGINES_ALL)
+    if (disp_name != 0x4e54532d44495350L):
+      raise Exception("WARNING: invalid dispatcher name: {}" % disp_name)
+    if (disp_version < 0x302e3032L):
+      raise Exception("WARNING: invalid dispatcher version: {}" % disp_version)
+    if (disp_engines < 1):
+      raise Exception("WARNING: invalid dispatcher engine count: {}" % disp_engines)
+    return disp_engines
 
 def dump_nts_dispatcher_api(api):
     for addr in range(0, 0x1000):
@@ -186,19 +197,16 @@ def dump_nts_dispatcher_api(api):
         if (value != 0):
             print("dispatcher[%03x] = %08x" % (addr, value) );
 
-def dump_nts_engine_api(api):
+def dump_nts_engine_api(api, engine):
     for addr in range(0, 0x1000):
-        value = engine_read32(api, addr)
+        value = engine_read32(api, engine, addr)
         if (value != 0):
-            print("engine[%03x] = %08x" % (addr, value) );
+            print("engine%d[%03x] = %08x" % (engine, addr, value) );
 
 def check_nts_dispatcher_apis(api):
     print("Checking access to APIs in NTS")
     print("")
     print("")
-    #print("NAME0:       0x%08x" % read32(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_NAME))
-    #print("NAME1:       0x%08x" % read32(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_NAME + 1))
-    #print("VERSION:     0x%08x" % read32(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_VERSION))
     print("")
     print("Core:    %s" % human64(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_NAME))
     print("Version: %s" % human32(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_VERSION))
@@ -220,68 +228,69 @@ def check_nts_dispatcher_apis(api):
     print(" - BAD:        %d" % read64(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_COUNTER_BAD))
     print(" - DISPATCHED: %d" % read64(api, DISPATCHER_BASE, API_DISPATCHER_ADDR_COUNTER_DISPATCHED))
     print("")
-    print("ENGINE:");
-    print(" Core:    %s" % engine_human64(api, API_ADDR_ENGINE_NAME0))
-    print(" Core:    %s" % engine_human64(api, API_ADDR_CLOCK_NAME0))
-    print(" Core:    %s" % engine_human32(api, API_ADDR_DEBUG_NAME))
-    print(" Core:    %s" % engine_human64(api, API_ADDR_KEYMEM_NAME0))
-    print(" Core:    %s" % engine_human64(api, API_ADDR_NONCEGEN_NAME))
-    print(" Core:    %s" % engine_human64(api, API_ADDR_PARSER_NAME0))
-    print("")
-    print("ENGINE Debug");
-    print(" - NTS");
-    print("   - Processed:  %d" % engine_read64(api, API_ADDR_DEBUG_NTS_PROCESSED))
-    print("   - Bad cookie: %d" % engine_read64(api, API_ADDR_DEBUG_NTS_BAD_COOKIE))
-    print("   - Bad auth:   %d" % engine_read64(api, API_ADDR_DEBUG_NTS_BAD_AUTH))
-    print("   - Bad keyid:  %d" % engine_read64(api, API_ADDR_DEBUG_NTS_BAD_KEYID))
-    print(" - Error counters");
-    print("   - Crypto:     %d" % engine_read64(api, API_ADDR_DEBUG_ERR_CRYPTO))
-    print("   - TxBuf:      %d" % engine_read64(api, API_ADDR_DEBUG_ERR_TXBUF))
-    print(" - Other debug messurements:")
-    print("   - Systick32:  %d" % engine_read32(api, API_ADDR_DEBUG_SYSTICK32))
-    print("")
-    print("Parser");
-    print(" - State:         0x%0x" % engine_read32(api, API_ADDR_PARSER_STATE));
-    print(" - State Crypto:  0x%0x" % engine_read32(api, API_ADDR_PARSER_STATE_CRYPTO))
-    print(" - Error State:   0x%0x" % engine_read32(api, API_ADDR_PARSER_ERROR_STATE))
-    print(" - Error Counter: %0d" % engine_read32(api, API_ADDR_PARSER_ERROR_COUNT))
+
+def check_nts_engine_apis(api, engine):
+    print("ENGINE %x:" % engine);
+    print("  - Cores:")
+    print("   - Core:  %s" % engine_human64(api, engine, API_ADDR_ENGINE_NAME0))
+    print("   - Core:  %s" % engine_human64(api, engine, API_ADDR_CLOCK_NAME0))
+    print("   - Core:  %s" % engine_human32(api, engine, API_ADDR_DEBUG_NAME))
+    print("   - Core:  %s" % engine_human64(api, engine, API_ADDR_KEYMEM_NAME0))
+    print("   - Core:  %s" % engine_human64(api, engine, API_ADDR_NONCEGEN_NAME))
+    print("   - Core:  %s" % engine_human64(api, engine, API_ADDR_PARSER_NAME0))
+    print("  - Debug");
+    print("   - NTS");
+    print("     - Processed:  %d" % engine_read64(api, engine, API_ADDR_DEBUG_NTS_PROCESSED))
+    print("     - Bad cookie: %d" % engine_read64(api, engine, API_ADDR_DEBUG_NTS_BAD_COOKIE))
+    print("     - Bad auth:   %d" % engine_read64(api, engine, API_ADDR_DEBUG_NTS_BAD_AUTH))
+    print("     - Bad keyid:  %d" % engine_read64(api, engine, API_ADDR_DEBUG_NTS_BAD_KEYID))
+    print("   - Error counters");
+    print("     - Crypto:     %d" % engine_read64(api, engine, API_ADDR_DEBUG_ERR_CRYPTO))
+    print("     - TxBuf:      %d" % engine_read64(api, engine, API_ADDR_DEBUG_ERR_TXBUF))
+    print("   - Other debug messurements:")
+    print("     - Systick32:  %d" % engine_read32(api, engine, API_ADDR_DEBUG_SYSTICK32))
+    print("  - Parser");
+    print("    - State:         0x%0x" % engine_read32(api, engine, API_ADDR_PARSER_STATE));
+    print("    - State Crypto:  0x%0x" % engine_read32(api, engine, API_ADDR_PARSER_STATE_CRYPTO))
+    print("    - Error State:   0x%0x" % engine_read32(api, engine, API_ADDR_PARSER_ERROR_STATE))
+    print("    - Error Counter: %0d" % engine_read32(api, engine, API_ADDR_PARSER_ERROR_COUNT))
     print("")
 
-def nts_init_noncegen(api):
+def nts_init_noncegen(api, engine):
     print("Init nonce generator")
-    engine_write32( api, API_ADDR_NONCEGEN_KEY0, 0x5eb63bbb); # TODO: Seed noncegen with Randomness
-    engine_write32( api, API_ADDR_NONCEGEN_KEY1, 0xe01eeed0);
-    engine_write32( api, API_ADDR_NONCEGEN_KEY2, 0x93cb22bb);
-    engine_write32( api, API_ADDR_NONCEGEN_KEY3, 0x8f5acdc3);
-    engine_write32( api, API_ADDR_NONCEGEN_CONTEXT0, 0x6adfb183);
-    engine_write32( api, API_ADDR_NONCEGEN_CONTEXT1, 0xa4a2c94a);
-    engine_write32( api, API_ADDR_NONCEGEN_CONTEXT2, 0x2f92dab5);
-    engine_write32( api, API_ADDR_NONCEGEN_CONTEXT3, 0xade762a4);
-    engine_write32( api, API_ADDR_NONCEGEN_CONTEXT4, 0x7889a5a1);
-    engine_write32( api, API_ADDR_NONCEGEN_CONTEXT5, 0xdeadbeef);
-    engine_write32( api, API_ADDR_NONCEGEN_LABEL, 0x00000000); # TODO: Use 16B engine counter as nonce label
-    engine_write32( api, API_ADDR_NONCEGEN_CTRL, 0x00000001);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_KEY0, 0x5eb63bbb); # TODO: Seed noncegen with Randomness
+    engine_write32( api, engine, API_ADDR_NONCEGEN_KEY1, 0xe01eeed0);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_KEY2, 0x93cb22bb);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_KEY3, 0x8f5acdc3);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_CONTEXT0, 0x6adfb183);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_CONTEXT1, 0xa4a2c94a);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_CONTEXT2, 0x2f92dab5);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_CONTEXT3, 0xade762a4);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_CONTEXT4, 0x7889a5a1);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_CONTEXT5, 0xdeadbeef);
+    engine_write32( api, engine, API_ADDR_NONCEGEN_LABEL, 0x00000000); # TODO: Use 16B engine counter as nonce label
+    engine_write32( api, engine, API_ADDR_NONCEGEN_CTRL, 0x00000001);
 
-def nts_disable_keys(api):
-    print("Disable NTS keys")
-    engine_write32_checkreadback( api, API_ADDR_KEYMEM_ADDR_CTRL, 0 )
+def nts_disable_keys(api, engine):
+    print("Engine %d - Disable NTS keys" % engine)
+    engine_write32_checkreadback( api, engine, API_ADDR_KEYMEM_ADDR_CTRL, 0 )
 
-def nts_set_current_key(api, keynum):
-    print("Set current NTS key: %x" % keynum);
-    print(" * key ctrl: %08x" % engine_read32( api, API_ADDR_KEYMEM_ADDR_CTRL ))
+def nts_set_current_key(api, engine, keynum):
+    print("Engine %d - Set current NTS key: %x" % (engine, keynum))
+    print(" * key ctrl: %08x" % engine_read32( api, engine, API_ADDR_KEYMEM_ADDR_CTRL ))
     CTRL_CURR_LOW = 16
     ctrl = 0
     mask1 = 0x3<<CTRL_CURR_LOW
     mask2 = 0xffffffff ^ mask1;
     k = 0
     k = (keynum & 0x3) << CTRL_CURR_LOW
-    ctrl = engine_read32( api, API_ADDR_KEYMEM_ADDR_CTRL )
+    ctrl = engine_read32( api, engine, API_ADDR_KEYMEM_ADDR_CTRL )
     ctrl = ctrl & mask2
     ctrl = ctrl | k
-    engine_write32_checkreadback( api, API_ADDR_KEYMEM_ADDR_CTRL, ctrl )
-    print(" * key ctrl: %08x" % engine_read32( api, API_ADDR_KEYMEM_ADDR_CTRL ))
+    engine_write32_checkreadback( api, engine, API_ADDR_KEYMEM_ADDR_CTRL, ctrl )
+    print(" * key ctrl: %08x" % engine_read32( api, engine, API_ADDR_KEYMEM_ADDR_CTRL ))
 
-def nts_install_key_256bit(api, key_index, keyid, key=[]):
+def nts_install_key_256bit(api, engine, key_index, keyid, key=[]):
     addr_key = 0
     addr_keyid = 0
     addr_length = 0
@@ -295,31 +304,31 @@ def nts_install_key_256bit(api, key_index, keyid, key=[]):
 
     ( addr_key, addr_keyid, addr_length ) = dictionary.get( key_index )
 
-    print("Install key, index = %x, address key = %x, address key id = %x, address key length = %x" % (key_index, addr_key, addr_keyid, addr_length))
+    print("Engine %d - Install key, index = %x, address key = %x, address key id = %x, address key length = %x" % (engine, key_index, addr_key, addr_keyid, addr_length))
 
-    ctrl = engine_read32( api, API_ADDR_KEYMEM_ADDR_CTRL )
+    ctrl = engine_read32( api, engine, API_ADDR_KEYMEM_ADDR_CTRL )
     ctrl = ctrl & ~ (1<<key_index);
 
-    engine_write32_checkreadback( api, API_ADDR_KEYMEM_ADDR_CTRL, ctrl )
-    print(" * key ctrl: %08x" % engine_read32( api, API_ADDR_KEYMEM_ADDR_CTRL ))
+    engine_write32_checkreadback( api, engine, API_ADDR_KEYMEM_ADDR_CTRL, ctrl )
+    print(" * key ctrl: %08x" % engine_read32( api, engine, API_ADDR_KEYMEM_ADDR_CTRL ))
 
-    engine_write32_checkreadback( api, addr_keyid, keyid )
-    engine_write32_checkreadback( api, addr_length, 0 )
+    engine_write32_checkreadback( api, engine, addr_keyid, keyid )
+    engine_write32_checkreadback( api, engine, addr_length, 0 )
 
     for i in range(0, 8):
        addr = addr_key + i
        value = key[7-i]
        print(" * key[%x,%x]=engine[%x]=%x" % (key_index, i, addr, value))
-       engine_write32_checkreadback( api, addr,       value ) #256bit LSB
-       engine_write32_checkreadback( api, addr + 0x8, 0     ) #256bit MSB, all zeros
+       engine_write32_checkreadback( api, engine, addr,       value ) #256bit LSB
+       engine_write32_checkreadback( api, engine, addr + 0x8, 0     ) #256bit MSB, all zeros
 
    #for i in range(7, -1, -1):
    #   addr = addr_key + i
    #   print(" * key[%d]: %08x" % (i, engine_read32( api, addr )))
 
     ctrl = ctrl | (1<<key_index)
-    engine_write32_checkreadback( api, API_ADDR_KEYMEM_ADDR_CTRL, ctrl )
-    print(" * key ctrl: %08x" % engine_read32( api, API_ADDR_KEYMEM_ADDR_CTRL ))
+    engine_write32_checkreadback( api, engine, API_ADDR_KEYMEM_ADDR_CTRL, ctrl )
+    print(" * key ctrl: %08x" % engine_read32( api, engine, API_ADDR_KEYMEM_ADDR_CTRL ))
 
 
 #-------------------------------------------------------------------
@@ -328,14 +337,23 @@ if __name__=="__main__":
     api = api_extension(path)
 
     dump_nts_dispatcher_api(api)
-    dump_nts_engine_api(api)
-    check_version_board()
-    check_nts_dispatcher_apis(api)
+    engines = detect_engines(api)
 
-    nts_disable_keys(api);
-    nts_init_noncegen(api);
-    nts_install_key_256bit(api, 0, 0x12345678, [ 0x9b71d224, 0xbd62f378, 0x5d96d46a, 0xd3ea3d73, 0x319bfbc2, 0x890caada, 0xe2dff725, 0x19673ca7 ] )
-    nts_install_key_256bit(api, 1, 0xb01dface, [ 0x2323c3d9, 0x9ba5c11d, 0x7c7acc6e, 0x14b8c5da, 0x0c466347, 0x5c2e5c3a, 0xdef46f73, 0xbcdec043 ] )
-    nts_install_key_256bit(api, 2, 0x13fe78e9, [ 0xfeb10c69, 0x9c6435be, 0x5a9ee521, 0xe40e420c, 0xf665d8f7, 0xa969302a, 0x63b9385d, 0x353ae43e ] )
-    nts_set_current_key(api, 1)
+    for engine in range(0, engines):
+      dump_nts_engine_api(api, engine)
+
+    check_version_board()
+
+    for engine in range(0, engines):
+      nts_disable_keys(api, engine)
+      nts_init_noncegen(api, engine)
+      nts_install_key_256bit(api, engine, 0, 0x12345678, [ 0x9b71d224, 0xbd62f378, 0x5d96d46a, 0xd3ea3d73, 0x319bfbc2, 0x890caada, 0xe2dff725, 0x19673ca7 ] )
+      nts_install_key_256bit(api, engine, 1, 0xb01dface, [ 0x2323c3d9, 0x9ba5c11d, 0x7c7acc6e, 0x14b8c5da, 0x0c466347, 0x5c2e5c3a, 0xdef46f73, 0xbcdec043 ] )
+      nts_install_key_256bit(api, engine, 2, 0x13fe78e9, [ 0xfeb10c69, 0x9c6435be, 0x5a9ee521, 0xe40e420c, 0xf665d8f7, 0xa969302a, 0x63b9385d, 0x353ae43e ] )
+      nts_set_current_key(api, engine, 1)
+
+    check_nts_dispatcher_apis(api)
+    for engine in range(0, engines):
+      check_nts_engine_apis(api, engine)
+
     sys.exit(0)
