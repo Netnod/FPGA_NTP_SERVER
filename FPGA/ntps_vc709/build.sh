b22afdd3d1289eaeb8efd8c2b4af852fded1395c
Christer Weinigel <wingel@netnod.se>
2020-10-26 11:59:06 +0100

    Remove programming files before building and verify that they exist after

--- FPGA/ntps_vc709/build.sh
+++ FPGA/ntps_vc709/build.sh
@@ -9,11 +9,11 @@ cd xilinx
 ./download-and-unpack.sh
 )
 
-# rm -f ntps_top.bit ntps_top.mcs -f ntps_top.prm
+rm -f ntps_top.bit ntps_top.mcs -f ntps_top.prm
 
 time vivado -mode batch -source build.tcl
 
 # Verify that all programming files exist
-# [ -f ntps_top.bit ]
-# [ -f ntps_top.mcs ]
-# [ -f ntps_top.prm ]
+[ -f ntps_top.bit ]
+[ -f ntps_top.mcs ]
+[ -f ntps_top.prm ]
