e5809f2b6c9bba7cb0ab6003fabaf62478e207d8
Christer Weinigel <wingel@netnod.se>
2020-10-30 13:52:25 +0100

    Update programming scripts for VCU118

--- FPGA/ntps_vcu118/program_fpga.sh
+++ FPGA/ntps_vcu118/program_fpga.sh
@@ -1,12 +1,29 @@
 #! /bin/bash
-set -e
 
-if [ -z "`pgrep hw_server`" ]; then
-    echo "Starting hw_server daemon"
-    echo
-    hw_server -d
+BIT_FILE=ntps_top.bit
+
+echo "num" $#
+
+if [ $# -gt 1 ]; then
+    BIT_FILE="$1"
 fi
 
-echo "Programming FPGA with ntps_top.bit"
-echo
-vivado -mode batch -source program_fpga.tcl
+. /opt/Xilinx/Vivado_Lab/2019.2/settings64.sh
+
+set -x
+set -e
+
+lspci -d 10ee:
+PCI_NODE=`lspci -d 10ee: | awk '{print $1}'`
+PCI_DEV=0000:$PCI_NODE
+
+if [ ! -z "$PCI_NODE" ]; then
+    cat /sys/bus/pci/devices/$PCI_DEV/{vendor,device}
+    echo 1 >/sys/bus/pci/devices/$PCI_DEV/remove
+fi
+
+vivado_lab -mode batch -source program_fpga.tcl -tclargs "$BIT_FILE"
+
+echo 1 >/sys/bus/pci/rescan
+
+lspci -vnn -d 10ee:
