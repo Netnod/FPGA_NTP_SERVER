f52536729ee59aa567b8d30b58dc84bcfc36df09
Christer Weinigel <wingel@netnod.se>
2020-11-19 12:16:02 +0100

    Use same program_fpga.sh and reload_fpg.sh scripts as vcu118

--- FPGA/targets/ntps_vc709/program_fpga.sh
+++ FPGA/targets/ntps_vc709/program_fpga.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
+
+BIT_FILE=ntps_top.bit
+
+echo "num" $#
+
+if [ $# -ge 1 ]; then
+    BIT_FILE="$1"
+fi
+
+. /opt/Xilinx/Vivado_Lab/2019.2/settings64.sh
+
+set -x
 set -e
 
 if [ -z "`pgrep hw_server`" ]; then
@@ -7,6 +19,17 @@ if [ -z "`pgrep hw_server`" ]; then
     hw_server -d
 fi
 
-echo "Programming FPGA with ntps_top.bit"
-echo
-vivado -mode batch -source program_fpga.tcl
+lspci -d 10ee:
+PCI_NODE=`lspci -d 10ee: | awk '{print $1}'`
+PCI_DEV=0000:$PCI_NODE
+
+if [ ! -z "$PCI_NODE" ]; then
+    cat /sys/bus/pci/devices/$PCI_DEV/{vendor,device}
+    echo 1 >/sys/bus/pci/devices/$PCI_DEV/remove
+fi
+
+vivado_lab -mode batch -source program_fpga.tcl -tclargs "$BIT_FILE"
+
+echo 1 >/sys/bus/pci/rescan
+
+lspci -vnn -d 10ee:
---
+++ FPGA/targets/ntps_vc709/reload_fpga.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -x
+set -e
+
+cd "`dirname \"$0\"`"
+
+(cd ../../../Linux_Driver && make clean && make || exit 1)
+
+sudo ./program_fpga.sh "$@"
+
+(cd ../../../Linux_Driver && sudo ./setup_dev.sh)
