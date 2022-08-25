f0b4fa3ea756a635cd01d92e6d254090c626e24c
David Snowdon <daves@metamako.com>
2022-08-25 17:01:44 +1000

    Change to pipefail to ensure failure if the makefile fails.

--- FPGA/targets/ntps_arista/build.sh
+++ FPGA/targets/ntps_arista/build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-set -x
-set -e
+set -euxo pipefail
 
 ARISTA_FDK_DIR=${ARISTA_FDK_DIR:-$(pwd)/../../../arista_fdk-2.0.0beta1}
 echo "Building using Arista FDK in $ARISTA_FDK_DIR"
@@ -8,7 +7,7 @@ echo "Building using Arista FDK in $ARISTA_FDK_DIR"
 (cd ../../cores && ./download-and-unpack.sh || exit 1)
 git clean -fdxq
 /bin/time nice make ARISTA_FDK_DIR=$ARISTA_FDK_DIR BOARDSTD=lb2 2>&1 | tee log
-if ! test -f ntps-3.0.0.x86_64.rpm; then
+if ! test -f ntps-3.0.0.swix; then
     echo "Build failed" 1>&2
     exit 1
 fi
