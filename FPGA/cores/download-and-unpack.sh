6aeea745b843cc700b79cf41ad55e5665f42a134
David Charles Ambler Snowdon <daves+git@arista.com>
2023-03-09 15:43:41 +1100

    Feature/deveco 770 build ntp server as fdk app (#2)
    * Pass through the ARISTA_FDK_DIR variable so we can build with an FDK in an arbitrary directory.
    
    * Add rules to copy files from the FDK, rather than use symlinks.
    
    * Update to use Netnod forks of these projects.
    
    * Change to using a .swix rather than a .rpm since this works better on EOS.
    
    * Change to pipefail to ensure failure if the makefile fails.
    
    * Remove a trailing comma in the .json file.
    
    * Make wget more quiet for use in CI.

--- FPGA/cores/download-and-unpack.sh
+++ FPGA/cores/download-and-unpack.sh
@@ -5,7 +5,7 @@ DOWNLOADS=../../downloads
 
 if [ ! -f v7_xt_conn_trd/readme.txt ]; then
     mkdir -p $DOWNLOADS
-    wget -nc -P $DOWNLOADS https://www.xilinx.com/support/documentation/boards_and_kits/vc709/2014_3/rdf0285-vc709-connectivity-trd-2014-3.zip
+    wget -nv -nc -P $DOWNLOADS https://www.xilinx.com/support/documentation/boards_and_kits/vc709/2014_3/rdf0285-vc709-connectivity-trd-2014-3.zip
     rm -rf v7_xt_conn_trd
     unzip -q $DOWNLOADS/rdf0285-vc709-connectivity-trd-2014-3.zip
 fi
